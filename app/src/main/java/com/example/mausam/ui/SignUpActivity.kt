2d6bdd52d9a1f7970e203d6a4bd00137559fa500
Madhav Sharma <sharmamadhav208@gmail.com>
2023-01-05 18:53:33 +0530

    "Login,Signup and splashscreen Activities added with barebone layout,"

---
+++ app/src/main/java/com/example/mausam/ui/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mausam.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mausam.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
--- app/src/main/java/com/example/mausam/ui/SplashActivity.kt
+++ app/src/main/java/com/example/mausam/ui/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mausam
+package com.example.mausam.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mausam.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
