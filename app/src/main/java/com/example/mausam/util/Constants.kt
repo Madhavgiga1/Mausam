1d84d890a8ae4dd107993da97314f5271d755456
Madhav Sharma <sharmamadhav208@gmail.com>
2023-01-08 21:57:20 +0530

    "sensitive info deleted "

--- app/src/main/java/com/example/mausam/util/Constants.kt
+++ app/src/main/java/com/example/mausam/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.mausam.util
 class Constants {
     companion object{
         const val BASE_URL="http://api.weatherapi.com/v1"
-        const val API_KEY="58ef069237a44734ac4111055230501"
+        const val API_KEY="0aa3d9af63474bcea89161322230801"
 
 
 
